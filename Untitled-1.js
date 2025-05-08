18665bdf1ed9fc2ad1c4a0672cabf4e6c436ea6d
Omans16 <145634073+Omans16@users.noreply.github.com>
2025-05-08 11:29:15 +0700

    Delete Untitled-1.js

--- Untitled-1.js
+++
@@ -1,9 +0,0 @@
-const { ethers } = require("ethers");
-
-async function getPublicKey() {
-    const provider = new ethers.providers.JsonRpcProvider("https://mainnet.infura.io/v3/YOUR_INFURA_PROJECT_ID");
-    const wallet = new ethers.Wallet("PRIVATE_KEY_HERE", provider);
-    console.log("Public Key:", wallet.publicKey);
-}
-
-getPublicKey();
